7304d7739bc49981d44c9b4a9029a8ff2cf27006
Rik <rikvanrijn2001@gmail.com>
2023-04-02 19:12:51 +0200

    moved DemoPresentation to Presentation class

--- src/JabberPoint.java
+++ src/JabberPoint.java
@@ -29,7 +29,7 @@ public class JabberPoint {
 		new SlideViewerFrame(JABVERSION, presentation);
 		try {
 			if (argv.length == 0) { //a demo presentation
-				DemoPresentation.createDemoPresentation(presentation);
+				presentation.createDemoPresentation(presentation);
 			} else {
 				new XMLParser().loadFile(presentation, argv[0]);
 			}
